a24a5f120b99fc95416c46e3200f0d9568b910b0
Lucino23 <58440306+Lucino23@users.noreply.github.com>
2020-10-27 16:44:43 +0100

    Update Start_metingen.py

--- Scripts/Start_metingen.py
+++ Scripts/Start_metingen.py
@@ -7,7 +7,7 @@ try:
     conn = mariadb.connect(
     user="luca",
     password="Antluc0824",
-    host="192.168.0.118",
+    host="localhost",
     port=3306,
     database="PyTempProject"
     )
@@ -19,6 +19,6 @@ except mariadb.Error as e:
 cursor = conn.cursor()
 conn.autocommit = True
 query = """INSERT INTO Meting(Start_datum_tijd, Stop_datum_tijd, Avg_S1, Avg_S2, Avg_S3, Avg_S4, Average) VALUES(?,?,?,?,?,?,?);"""
-data = (datetime.now(), "NULL", "NULL", "NULL", "NULL", "NULL", "NULL")
+data = (datetime.now(),None,None,None,None,None,None)
 cursor.execute(query, data)
 conn.close()
